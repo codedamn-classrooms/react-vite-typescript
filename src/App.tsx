0b49729b6352696e80de91ee8033e57cc3bf3490
Pranav <pranav.techiegeek@gmail.com>
2022-10-12 00:31:07 +0530

    basic boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logos from "./assets/react-typescript.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={logos} alt="react typescript" width="256px" />;
+    </>
+  );
+}
+
+export default App;
