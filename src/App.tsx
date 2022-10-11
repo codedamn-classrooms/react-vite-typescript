3965a159c2f52c39bf3c80b8ce70df76b19da225
Pranav <pranav.techiegeek@gmail.com>
2022-10-12 00:43:48 +0530

    preview ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import logos from "./assets/react-typescript.png";
 import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <>
-      <img src={logos} alt="react typescript" width="256px" />;
+      <img
+        src="https://codedamn-website-assets.s3.amazonaws.com/uploads/oqBMX4cxj0HjvM9KMx3lN/react-typescript.png"
+        alt="react typescript"
+        width="256px"
+      />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h3>Powered by</h3>
+        <img
+          src="https://avatars.githubusercontent.com/u/37881195?s=280&v=4"
+          width="32px"
+        />
+      </div>
     </>
   );
 }
